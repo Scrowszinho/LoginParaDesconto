05012fbff437b096e788bf68c639be61190ee0f9
Luis Gustavo <gustascrows@gmail.com>
2021-01-29 13:12:35 -0300

    Feat: App atualizado

--- src/Connection.js
+++ src/Connection.js
@@ -3,24 +3,51 @@ import { SafeAreaView, View, TextInput, Text, Button } from 'react-native';
 import { StyleSheet } from 'react-native';
 export default function Connection() {
   const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
+  const [password, setPassword] = useState('');
+  const [cupom, setCupom] = useState(false);
+  const [status, setStatus] = useState('');
 
   const handleClicker = async () => {
-    
+    const req = fetch('https://api.b7web.com.br/loginsimples/',{
+    method:'POST',
+    body:JSON.stringify({email,password}),
+    headers:{
+      'Content-Type':'application/json'
+    }  
+  })
+  const json = await (await req).json();
+  if(json.status === 'ok'){
+    setStatus('Acesso Liberado');
+    setCupom(true);
+  }else{
+    setStatus('Acesso Negado');
+    setCupom(false);
+  }
   }
   return (
     <SafeAreaView style={styles.container}>
-
         <Text style={styles.header}>Desconto é aqui</Text>
-        <TextInput style={styles.input}/>
-        <TextInput style={styles.input}/>
-        <Button title='Verificar' />
-        <Text style={styles.status}>...</Text>
+        <TextInput style={styles.input}
+        value={email} placeholder='Digite seu email'
+        onChangeText={t=>setEmail(t)}
+        placeholderTextColor='#111'
+        />
+        <TextInput style={styles.input}
+        value={password} placeholder='Digite sua senha'
+        onChangeText={t=>setPassword(t)}
+        placeholderTextColor='#111'
+        secureTextEntry={true}
+        />
+        <Button title='Verificar' 
+        onPress={handleClicker}
+        />
+        <Text style={styles.status}>{status}</Text>
+        {cupom &&
         <View style={styles.cupomArea}>
           <Text style={styles.cupomTitle}>Código do Cupom:</Text>
           <Text style={styles.cupom}>JAHKR1337</Text>
         </View>
-
+      }
     </SafeAreaView>
   );
 
